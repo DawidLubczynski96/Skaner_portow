1aef2dabc893c7822c58bac9a595078fd528431b
student <student@PC-205>
2017-05-25 18:44:46 +0200

    Gotowy skaner

---
+++ Skaner_portow/Skaner_portow/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Skaner_portow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.przycisk_skanuj = new System.Windows.Forms.Button();
+            this.adres = new System.Windows.Forms.TextBox();
+            this.info = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // przycisk_skanuj
+            // 
+            this.przycisk_skanuj.Location = new System.Drawing.Point(32, 338);
+            this.przycisk_skanuj.Name = "przycisk_skanuj";
+            this.przycisk_skanuj.Size = new System.Drawing.Size(398, 39);
+            this.przycisk_skanuj.TabIndex = 0;
+            this.przycisk_skanuj.Text = "Skanuj";
+            this.przycisk_skanuj.UseVisualStyleBackColor = true;
+            this.przycisk_skanuj.Click += new System.EventHandler(this.przycisk_skanuj_Click);
+            // 
+            // adres
+            // 
+            this.adres.Location = new System.Drawing.Point(32, 23);
+            this.adres.Name = "adres";
+            this.adres.Size = new System.Drawing.Size(398, 20);
+            this.adres.TabIndex = 1;
+            // 
+            // info
+            // 
+            this.info.FormattingEnabled = true;
+            this.info.Location = new System.Drawing.Point(32, 67);
+            this.info.Name = "info";
+            this.info.Size = new System.Drawing.Size(398, 251);
+            this.info.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 398);
+            this.Controls.Add(this.info);
+            this.Controls.Add(this.adres);
+            this.Controls.Add(this.przycisk_skanuj);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button przycisk_skanuj;
+        private System.Windows.Forms.TextBox adres;
+        private System.Windows.Forms.ListBox info;
+    }
+}
+
---
+++ Skaner_portow/Skaner_portow/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Skaner_portow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void przycisk_skanuj_Click(object sender, EventArgs e)
+        {
+            short[] ListaPortow = { 20, 21, 22, 80, 111, 30000 };
+            string host = adres.Text;
+            info.Items.Add("Skanowanie portów dla " + host);
+            info.Items.Add("To może chwilę potrwać... ");
+            foreach (short port in ListaPortow)
+            {
+                this.Refresh();
+                try
+                {
+                    TcpClient klient = new TcpClient(host, port);
+                    info.Items.Add("Port: " + port.ToString() + "jest otwarty");
+                }
+                catch
+                {
+                    info.Items.Add("Port: " + port.ToString() + "jest zamknięty ");
+                }
+            }
+        }
+    }
+}
